17acca22cdeea301be46157d57d7081c6927f2af
Zaikhul Sulthon Suasono <zaikhulsulthon@gmail.com>
2024-04-20 22:36:43 +0700

    ocr rooms schemas

---
+++ src/schemas/ocr.schema.ts
@@ -0,0 +1,20 @@
+export interface OCR_ClassRoom {
+  id: number;
+  room_code: number;
+  room_name: string;
+  position: string;
+}
+
+export interface OCR_LabRoom {
+  id: number;
+  Lab_code: number;
+  Lab_name: string;
+  position: string;
+}
+
+export interface OCR_LactureRoom {
+  id: number;
+  Lacture_code: number;
+  Lacture_name: string;
+  position: string;
+}
